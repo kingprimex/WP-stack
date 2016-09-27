bf71961736130b9d31effce88c0951749a2a5c72
kingprimex <dark_vader_n70@yahoo.com>
2016-09-27 14:19:10 +0300

    script almost perfect

--- webSetup.sh
+++ webSetup.sh
@@ -5,6 +5,7 @@ NGINX=$(dpkg -l | grep nginx)
 PHP=$(dpkg -l | grep php5)
 
 
+
 if [ "$MYSQL" == "" ]
 then
 
@@ -26,3 +27,44 @@ then
 
 fi
 
+read -p "Please enter your site name: " siteName
+
+echo "127.0.0.1 $siteName" >> /etc/hosts
+
+nginxRoot="/usr/share/nginx/html/$siteName"
+nginxConf="server { \n
+        listen 80 default_server;\n
+        root /usr/share/nginx/html/$siteName;\n
+        index index.html index.htm;\n
+
+        server_name $siteName;\n
+
+        location / {\n
+                try_files \$uri \$uri/ =404;\n
+        }\n
+}\n"
+
+`touch /etc/nginx/sites-available/$siteName.conf && echo -e $nginxConf >> /etc/nginx/sites-available/$siteName.conf && ln -s /etc/nginx/sites-available/$siteName.conf /etc/nginx/sites-enabled/$siteName.conf`
+
+`wget -O /tmp/wordpress.zip https://wordpress.org/latest.zip && unzip -q /tmp/wordpress.zip -d  /tmp/ && mv /tmp/wordpress /usr/share/nginx/html/$siteName`
+
+read -p "Please enter your Mysql password: " rootpass
+PASSWDDB="$(openssl rand -base64 12)"
+
+mainDB="$siteName"_db
+mainDB_user="$siteName"_user
+mysql --pasword=$rootpass -e "CREATE DATABASE $mainDB"
+mysql --password=$rootpass -e "GRANT ALL PRIVILEGES ON $mainDB.* TO $mainDB_user@localhost IDENTIFIED BY '$PASSWDDB'"
+
+wp_conf="define( 'DB_NAME', '"$mainDB"' );\n
+
+define( 'DB_USER', '"$mainDB_user"' );\n
+
+define( 'DB_PASSWORD', '"$PASSWDDB"' );\n
+
+define( 'DB_HOST', 'localhost' );\n"
+
+`mv /usr/share/nginx/html/$siteName/wp-config-sample.php /usr/share/nginx/html/$siteName/wp-config.php && echo -e $wp_conf >> /usr/share/nginx/html/$siteName/wp-config.php`
+
+
+`/etc/nginx/sbin/nginx -s reload`
